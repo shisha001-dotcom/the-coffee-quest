3718dde030d42086eb18f0d08cf6d3d6c46e8093
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-14 18:00:10 +0700

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,33 @@
+async function loadPage(pageName) {
+
+  const app = document.getElementById("app");
+
+  try {
+
+    const response = await fetch(`pages/${pageName}.html`);
+
+    const html = await response.text();
+
+    app.innerHTML = html;
+
+    window.scrollTo(0, 0);
+
+    // INIT BOARDGAME PAGE
+    if(pageName === "boardgame"){
+      initBoardgame();
+    }
+
+  } catch (error) {
+
+    app.innerHTML = `
+      <div style="padding:40px">
+        <h2>Lỗi tải trang</h2>
+      </div>
+    `;
+
+    console.error(error);
+  }
+}
+
+// LOAD DEFAULT PAGE
+loadPage("news");
