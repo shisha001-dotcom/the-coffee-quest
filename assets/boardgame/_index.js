8265fa105900a11cb919a45f5a5c98bdf4fd0f6f
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-15 18:16:55 +0700

    Create _index.js

---
+++ assets/boardgame/_index.js
@@ -0,0 +1,15 @@
+// Thêm game mới: chỉ cần thêm tên file vào đây
+const BOARDGAME_FILES = [
+  "uno",
+  "ticket-to-ride",
+  "catan",
+  "pandemic",
+  "co-vua",
+  "monopoly",
+  "dixit",
+  "carcassonne",
+  "no-thanks",
+  "werewolf",
+  "clue",
+  "dua-rua"
+];
