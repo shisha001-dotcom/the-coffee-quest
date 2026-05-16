18a52f24b107b7529698b2929a5441d7cef563bb
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-16 23:48:56 +0700

    Update dua_rua.js

--- assets/boardgame/dua_rua.js
+++ assets/boardgame/dua_rua.js
@@ -6,7 +6,7 @@ GAMES.push({
   time: "15-25 phút",
   difficulty: "Dễ",
   color: "#3ca55c",
-  heroBg: "https://cf.geekdo-images.com/original/img/turtle-race-banner.jpg",
+  heroBg: "https://hstatic.net/936/1000019936/1/2016/7-8/h3.jpg",
   objective: "Bí mật điều khiển chú rùa của mình về đích đầu tiên bằng cách sử dụng các lá bài di chuyển.",
   setup: [
     "Xếp tất cả rùa tại ô xuất phát.",
