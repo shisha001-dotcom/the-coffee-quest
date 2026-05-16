f2c3feab76a644383fa58d32919d82d5f6fb070d
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-16 23:31:09 +0700

    update game {{ $json.filename }}

---
+++ assets/boardgame/love-letter.js
@@ -0,0 +1,38 @@
+GAMES.push({
+name: "Love Letter",
+category: "Deduction, Card Game",
+emoji: "❤️",
+players: "2-6",
+time: "15-20 phút",
+difficulty: "Dễ",
+color: "#A52A2A",
+heroBg: "https://cf.geekdo-images.com/pHZ_6vI6KjA3f0y_7VlIvg__hero/img/0-sP1rQ0xH5l3Hh8xYFwYl8ySg=/fit-in/1600x800/filters:strip_icc()/pic1344933.jpg",
+objective: "Là người chơi cuối cùng còn lại trong vòng hoặc có lá bài giá trị cao nhất khi hết bài rút để giành lấy Token Ưu Ái (Favor Token). Người đầu tiên thu thập đủ số Token Ưu Ái quy định sẽ thắng trò chơi.",
+setup: [
+"Xáo trộn tất cả 16 lá bài nhân vật và phát cho mỗi người chơi 1 lá bài úp xuống làm bài tẩy trên tay của họ. Phần còn lại của bộ bài sẽ được đặt úp xuống giữa bàn chơi để tạo thành chồng bài rút.",
+"Rút 1 lá bài từ chồng bài rút và đặt úp xuống, không được nhìn, ra ngoài trò chơi. Nếu có 2 người chơi, rút thêm 3 lá bài nữa và đặt ngửa ra ngoài trò chơi để tăng tính bất ngờ và khó đoán. Xác định số Token Ưu Ái cần để thắng: 7 token cho 2 người chơi, 5 cho 3 người, và 4 cho 4 người chơi.",
+"Người chơi nào gần đây nhất đã nhận được một lá thư tình hoặc có vẻ ngoài lãng mạn nhất sẽ là người chơi đầu tiên, sau đó lượt chơi sẽ tiếp tục theo chiều kim đồng hồ."
+],
+turn: [
+"Khi đến lượt mình, người chơi đầu tiên sẽ rút một lá bài từ chồng bài rút, khiến bạn có tổng cộng hai lá bài trên tay. Việc này giúp bạn có thêm lựa chọn chiến thuật cho lượt đi hiện tại.",
+"Sau khi rút bài, người chơi phải chọn một trong hai lá bài trên tay để chơi. Đặt lá bài đã chọn ngửa xuống trước mặt bạn và ngay lập tức thực hiện hiệu ứng được ghi trên lá bài đó. Lá bài còn lại trên tay sẽ được giữ kín.",
+"Nếu hiệu ứng của lá bài bạn vừa chơi khiến bạn bị loại khỏi vòng chơi hiện tại (ví dụ như chơi lá Princess, bị loại bởi Guard, hay bị ảnh hưởng bởi Baron và mất), bạn sẽ ngay lập tức loại bỏ tất cả các lá bài còn lại trên tay vào chồng bài bỏ của mình và không tham gia thêm vào vòng chơi này. Lượt chơi sau đó sẽ chuyển sang người chơi bên trái kế tiếp còn lại trong vòng."
+],
+win: "Người chơi đầu tiên thu thập đủ số Token Ưu Ái quy định (dựa trên số lượng người chơi) sẽ thắng trò chơi.",
+tips: [
+"Theo dõi cẩn thận các lá bài đã được chơi bởi đối thủ và những lá bài đã bị loại bỏ ra khỏi trò chơi. Thông tin này rất quan trọng để suy luận những lá bài nào có khả năng còn trên tay đối thủ, giúp bạn đưa ra những quyết định chính xác khi sử dụng lá Guard hoặc Baron.",
+"Đừng ngại sử dụng các lá bài như Guard để đoán bài đối thủ hoặc Priest để nhìn trộm bài. Việc thu thập thông tin sớm có thể tạo lợi thế lớn, cho phép bạn lên kế hoạch cho các lượt đi sau và loại bỏ đối thủ một cách hiệu quả hơn.",
+"Cân nhắc kỹ lưỡng khi giữ các lá bài có giá trị cao như Princess hoặc Countess. Mặc dù chúng có thể giúp bạn thắng vòng nếu giữ được đến cuối, nhưng việc bị buộc phải chơi chúng trong tình huống không mong muốn có thể dẫn đến thất bại. Hãy luôn có một kế hoạch dự phòng."
+],
+images: [
+{
+url: "https://cf.geekdo-images.com/gK9J-yM-j86T06qV9W6G9A__imagepage/img/qL_f74R0x6nU0eYw17X8h0J_s_8=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6509439.jpg",
+caption: "Người chơi đang chọn lá bài để thực hiện hành động trong Love Letter."
+},
+{
+url: "https://cf.geekdo-images.com/vHqjH87gR257vMvC482L5w__imagepage/img/q-rBfLd56Q8Yyv0lH0KjS_5tT4A=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3014136.jpg",
+caption: "Một số lá bài nhân vật quen thuộc trong Love Letter với hiệu ứng đặc trưng."
+}
+],
+youtubeUrl: "https://www.youtube.com/watch?v=R9jJ_w7rKjo"
+});
