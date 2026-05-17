7276e0a030d586b63697da3d6e20a4a42ecfa652
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-17 21:44:39 +0700

    update game {{ $json.filename }}

---
+++ assets/boardgame/captain-flip.js
@@ -0,0 +1,42 @@
+GAMES.push({
+name: "Captain Flip",
+category: "Gia đình, Cướp biển, May rủi, Thu thập bộ",
+emoji: "🏴‍☠️",
+players: "2-5",
+time: "20-30 phút",
+difficulty: "Dễ",
+color: "#3282B8",
+heroBg: "https://cf.geekdo-images.com/gK9qQ17jT-g3qH_bK6P90A__original/img/W2n-3079s-z_J8t11cM5bT8sI0=/0x0/filters:format(jpeg)/pic6793660.jpg",
+objective: "Thu thập các thẻ nhân vật và xây dựng đội ngũ cướp biển mạnh nhất trên thuyền của bạn để ghi được số điểm cao nhất.",
+setup: [
+"Mỗi người chơi nhận một Bảng Thuyền riêng và đặt nó trước mặt mình. Lấy tất cả thẻ Vàng và thẻ Kho Báu đặt thành các chồng bài riêng biệt ở giữa bàn chơi. Trộn kỹ tất cả các thẻ Nhân Vật, úp xuống, sau đó chia thành ba chồng bài rút có kích thước gần bằng nhau và đặt chúng gần khu vực giữa bàn.",
+"Mỗi người chơi rút một thẻ Nhân Vật từ một trong các chồng bài rút và đặt ngửa lên ô trống đầu tiên trên Bảng Thuyền của mình (ô có biểu tượng bàn tay) để bắt đầu xây dựng đội cướp biển.",
+"Người chơi nào gần đây nhất đã nói \"Arr!\" hoặc \"Ahoi!\" sẽ là người chơi đầu tiên, hoặc đơn giản là người chơi trẻ nhất sẽ bắt đầu trò chơi, sau đó lượt chơi sẽ tiếp tục theo chiều kim đồng hồ."
+],
+turn: [
+"Người chơi hiện tại chọn một trong ba chồng bài rút thẻ Nhân Vật đang úp. Sau đó, rút lá thẻ trên cùng của chồng bài đã chọn và xem mặt ngửa của nó.",
+"Người chơi có quyền lựa chọn giữ nguyên mặt thẻ đang hiện ra hoặc quyết định lật mặt thẻ còn lại (flip). Sau khi quyết định mặt thẻ cuối cùng, người chơi phải đặt thẻ Nhân Vật này vào một ô trống bất kỳ trên Bảng Thuyền của mình.",
+"Sau khi đặt thẻ, người chơi phải ngay lập tức kích hoạt hiệu ứng của nhân vật vừa đặt (nếu có). Các hiệu ứng có thể bao gồm nhận Vàng, Kho Báu, lật thẻ khác, hoặc thay đổi vị trí thẻ. Lượt chơi sau đó chuyển sang người chơi tiếp theo theo chiều kim đồng hồ."
+],
+win: "Trò chơi kết thúc ngay lập tức khi một người chơi lấp đầy tất cả năm ô trống trên Bảng Thuyền của mình HOẶC khi một trong ba chồng bài rút thẻ Nhân Vật bị hết. Người chơi tiến hành tính tổng điểm: cộng điểm từ các nhân vật trên Bảng Thuyền, giá trị của thẻ Vàng và Kho Báu đã thu thập được. Người chơi có tổng điểm cao nhất sẽ giành chiến thắng và trở thành thuyền trưởng vĩ đại nhất!",
+tips: [
+"Luôn cân nhắc kỹ lưỡng trước khi quyết định lật thẻ (flip). Mặt còn lại có thể mang lại nhân vật có hiệu ứng mạnh mẽ hơn cho chiến lược của bạn, nhưng cũng có thể là một nhân vật kém phù hợp hoặc thậm chí gây bất lợi. Đôi khi, giữ mặt thẻ ban đầu là lựa chọn an toàn và hiệu quả hơn.",
+"Xây dựng một chiến lược dài hạn cho Bảng Thuyền của bạn. Các nhân vật có thể tương tác với nhau hoặc tạo thành các bộ để mang lại điểm thưởng lớn khi kết thúc trò chơi. Đừng chỉ tập trung vào điểm số tức thì mà hãy nghĩ đến sự kết hợp tối ưu.",
+"Theo dõi các chồng bài rút thẻ và hành động của đối thủ. Nếu bạn thấy một chồng bài đang cạn dần, đó có thể là dấu hiệu để đẩy nhanh việc kết thúc trò chơi nếu chiến lược của bạn đang dẫn trước, hoặc cố gắng cướp các thẻ quan trọng trước khi chúng biến mất."
+],
+images: [
+{
+url: "https://cf.geekdo-images.com/w9-4_H0O4P8wS-q6X_8XkQ__original/img/q9-WJ8467gR57iQkR_D-zE89R1Q=/0x0/filters:format(jpeg)/pic6793665.jpg",
+caption: "Toàn cảnh bàn chơi với các thẻ nhân vật và bảng thuyền."
+},
+{
+url: "https://cf.geekdo-images.com/T09M9g8XvS3y7oDqgY3h1g__original/img/sH9uQ6W3q4nLh-Q00x4m-w4f69o=/0x0/filters:format(jpeg)/pic6793666.jpg",
+caption: "Các thẻ nhân vật với hai mặt khác nhau."
+},
+{
+url: "https://cf.geekdo-images.com/jW0U_8h7gq0w9jJ_w7gP3Q__original/img/v1d8Q0z8b8m0j8P0h2g2W1g9r6c=/0x0/filters:format(jpeg)/pic6793661.jpg",
+caption: "Mặt sau của hộp game Captain Flip."
+}
+],
+youtubeUrl: "https://www.youtube.com/watch?v=F0f-kU6L5u0"
+});
